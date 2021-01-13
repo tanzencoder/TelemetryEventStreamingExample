1631dbaa82de892d470c305209b2612f4d047dee
William Daugherty <wdaugherty@dataspringinc.com>
2021-01-12 21:25:08 -0500

    Initial

---
+++ TelemetryProducerConsoleApp/TelemetryProducer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelemetryProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
